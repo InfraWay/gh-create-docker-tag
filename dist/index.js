2d2e79f2c98ff68ff90837add6450781b4339b40
mihma <mihmapro@gmail.com>
2022-06-08 22:55:03 +0100

    added image name output

--- dist/index.js
+++ dist/index.js
@@ -8863,11 +8863,20 @@ const github = __nccwpck_require__(3134);
 
 try {
   const shaLength = core.getInput('sha-length');
+  const registry = core.getInput('registry-name');
+
   const ref = github.context.ref.split('/');
-  const sha = github.context.sha
-  const tag = ref[1] == 'tags' ? ref[2] : sha.substring(0, shaLength)
+  const sha = github.context.sha;
+  
+  const tag = ref[1] == 'tags' ? ref[2] : sha.substring(0, shaLength);
+  const name = registry + ':' + tag;
+
   console.log(`Tag: ${tag}`);
+  console.log(`Name: ${name}`);
+
   core.setOutput("tag", tag);
+  core.setOutput("name", name);
+
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const github = require('@actions/github');
 
 try {
   const shaLength = core.getInput('sha-length');
+  const registry = core.getInput('registry-name');
+
   const ref = github.context.ref.split('/');
-  const sha = github.context.sha
-  const tag = ref[1] == 'tags' ? ref[2] : sha.substring(0, shaLength)
+  const sha = github.context.sha;
+  
+  const tag = ref[1] == 'tags' ? ref[2] : sha.substring(0, shaLength);
+  const name = registry + ':' + tag;
+
   console.log(`Tag: ${tag}`);
+  console.log(`Name: ${name}`);
+
   core.setOutput("tag", tag);
+  core.setOutput("name", name);
+
 } catch (error) {
   core.setFailed(error.message);
 }
