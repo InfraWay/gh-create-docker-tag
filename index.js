261238479a801e9418dbba93e0b63b8e3015ad0a
Andrew Red <andrew@reddikh.com>
2022-06-09 11:47:08 +0300

    - fixed linting

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const shaLength = core.getInput('sha-length');
+  const shaLength = parseInt(core.getInput('sha-length'), 10);
   const registry = core.getInput('registry-name');
 
   const ref = github.context.ref.split('/');
   const sha = github.context.sha;
-  
-  const tag = ref[1] == 'tags' ? ref[2] : sha.substring(0, shaLength);
+
+  const tag = ref[1] === 'tags' ? ref[2] : sha.substring(0, shaLength);
   const name = registry + ':' + tag;
 
   console.log(`Tag: ${tag}`);
